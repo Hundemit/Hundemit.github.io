c084b7df1a05e5fdf1dcae4363a8fd56dfb3721d
Jan Hindemit <janhindemit1@gmail.com>
2024-03-14 13:25:59 +0100

    FIRST

---
+++ assets/chunks/PokemonDetails.C7UFwyV9.js
@@ -0,0 +1,1 @@
+const s="/repo/assets/pokedéxScreenshot.D6BRHMI1.png",o="/repo/assets/PokedéxSuchfuntkion.BlaDwTYd.png",e="/repo/assets/PokemonDetails.DxRQ4cFW.png";export{s as _,o as a,e as b};
---
+++ assets/chunks/SideScroller.BDCB1UR5.js
@@ -0,0 +1,1 @@
+const s="/repo/assets/GYFFHome.CUQpDluN.png",e="/repo/assets/GyffHeaderPHone.CAW2nxfv.gif",o="/repo/assets/SideScroller.7tYFzh4U.gif";export{s as _,e as a,o as b};
